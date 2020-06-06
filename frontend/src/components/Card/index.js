f11ef9e19d686db7ca59f8cd776887dc86f1c5d5
Cuong <cuong@cuong.local>
2020-06-06 12:09:27 +0700

    init

--- frontend/src/components/Card/index.js
+++ frontend/src/components/Card/index.js
@@ -2,10 +2,11 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const Card = ({ article }) => {
-  const imageUrl =
-    process.env.NODE_ENV !== "development"
-      ? article.image.url
-      : process.env.REACT_APP_BACKEND_URL + article.image.url;
+  const imageUrl = article.image.url
+  // const imageUrl =
+  //   process.env.NODE_ENV !== "development"
+  //     ? article.image.url
+  //     : process.env.REACT_APP_BACKEND_URL + article.image.url;
   return (
     <Link to={`/article/${article.id}`} className="uk-link-reset">
       <div className="uk-card uk-card-muted">
